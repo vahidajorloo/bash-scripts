935e699e7a2c505403c5243c744c11a0349bf623
vahidajorloo <93025958+vahidajorloo@users.noreply.github.com>
2024-12-15 10:27:27 +0330

    Create email-and-subnet-announce.sh
    in case of attack it sends an email.
    in case of ban or unban it will announce /24 as well as /32

---
+++ fastnetmon/email-and-subnet-announce.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#
+# This script will get following arguments from FastNetMon:
+#
+#  $1 IP of host which is under attack (incoming attack) or source of attack (outgoing attack)
+#  $2 Attack direction: incoming or outgoing
+#  $3 Attack bandwidth in packets per second
+#  $4 Attack action: ban or unban
+#
+
+email_notify="noc@tehranserver.ir"
+
+# For ban action we will receive attack details to stdin
+# Please do not remove "cat" command because
+# FastNetMon will crash in this case as it expects read of data from script side
+#
+
+ip="$1"
+subnet=$(echo "$ip" | awk -F. '{print $1 "." $2 "." $3 ".0"}')
+
+if [ "$4" = "ban" ]; then
+    # This action receives multiple statistics about attack's performance and attack's sample to stdin
+    /opt/fastnetmon-community/libraries/gobgp_3_12_0/gobgp global rib add $ip/24 community 12880:6762
+    cat | mail -s "FastNetMon Community: IP $1 blocked because $2 attack with power $3 pps" $email_notify;
+    
+    # Please add actions to run when we ban host
+    exit 0
+fi
+
+if [ "$4" = "unban" ]; then
+    # No details provided to stdin here
+    /opt/fastnetmon-community/libraries/gobgp_3_12_0/gobgp global rib del $ip/24 community 12880:6762
+    # Please add actions to run when we unban host
+    exit 0
+fi
